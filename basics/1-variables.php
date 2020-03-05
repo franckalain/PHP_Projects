5c026be3e424474f3b13e377a0e1e80829bec797
franckalain <k.franckalains@gmail.com>
2020-03-05 15:00:34 +0100

    starting PHP projects

---
+++ basics/1-variables.php
@@ -0,0 +1,10 @@
+<?php
+$daysInWeek = 7;
+$text = 'there are ' . $daysInWeek . 'days in a week';
+echo $text;
+
+// or
+
+// variable reassignment
+$daysInWeek = 8;
+echo $text = 'there are ' . $daysInWeek . 'days in a week';
